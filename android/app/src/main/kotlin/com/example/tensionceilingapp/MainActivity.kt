1cf5b66e2cf820f02ad362bf23b68d771ae0c5d5
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-19 01:01:11 +0300

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/tensionceilingapp/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.tensionceilingapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // Класс должен оставаться пустым для V2 Embedding
+}
