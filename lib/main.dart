9e59386224e369c1a7ed983e5941ee068ddd0492
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-19 15:28:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app.dart';
-import 'database/database_helper.dart';
-import 'services/auto_save_service.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  final databaseHelper = DatabaseHelper();
-  await databaseHelper.initDatabase();
-  
-  final autoSaveService = AutoSaveService();
-  
-  runApp(
-    MultiProvider(
-      providers: [
-        Provider<DatabaseHelper>(create: (_) => databaseHelper),
-        Provider<AutoSaveService>(create: (_) => autoSaveService),
-      ],
-      child: const MyApp(),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -29,13 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PotolokForLife',
+      title: 'Tension Ceiling App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        useMaterial3: true,
       ),
-      home: const HomeScreen(),
-      debugShowCheckedModeBanner: false,
+      // Убираем const перед HomeScreen(), чтобы избежать ошибки
+      home: HomeScreen(),
     );
   }
 }
