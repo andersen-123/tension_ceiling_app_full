7ca3c285071f5665f0bd2469e4f2a657d647abfe
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-20 13:16:38 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Сметы потолков')),
+      appBar: AppBar(title: const Text('СМЕТЫ v1.0.9 - ЭТАП 4')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
